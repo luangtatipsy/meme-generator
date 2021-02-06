a045832d1821fb951122582aec7deb8f0697140f
Taechawat Konkaew <luangta_5@hotmail.com>
2021-02-06 14:34:55 +0700

    Add source code

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from './components/App'
+
+import 'normalize.css'
+import './assets/style.css'
+
+ReactDOM.render(<App />, document.getElementById("root"))
